73468d246fa26a945f626744177159deaecfb7a7
Heldeg <edhernandezfo@unal.edu.co>
2023-02-20 18:37:02 -0500

    strings

---
+++ src/es6/02-strings.js
@@ -0,0 +1,18 @@
+let hello = 'Hello';
+let world = 'world';
+let epicPhrase = hello + ' ' + world + '!';
+
+//Template literals
+// usa comillas francesas
+let epicPhrase2 =  `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+// multi-line strings
+
+let lorem = 'esto es un string \n' +'esto es otra linea';
+
+let lorem2 = `Esta es una frase epica
+esta es la continuacion de la frase
+`;
+console.log(lorem)
+console.log(lorem2);
