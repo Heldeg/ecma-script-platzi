0224c75c4af9281ee9ff895e4f024eb29e50f485
Heldeg <edhernandezfo@unal.edu.co>
2023-02-25 15:38:45 -0500

    trainding comas and string padding

---
+++ src/es8/03-string-padding.js
@@ -0,0 +1,6 @@
+//Hace que el string tenga de tamaño el número indicado añadiendo caracteres de la otra cadena
+const string = 'Hello';
+
+console.log(string.padStart(20, 'hi'));
+
+console.log(string.padEnd(6,'_'));
