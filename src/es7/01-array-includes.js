0f603c097dc446389d45c778a5851e192a5fe30b
Heldeg <edhernandezfo@unal.edu.co>
2023-02-24 20:32:55 -0500

    es7, object entries and obcject values

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,9 @@
+let numbers = [1, 2, 3 , 5 ,6]
+
+console.log(numbers.includes(3));
+
+console.log(numbers.includes(7));
+
+const names = ['oscar', 'David', 'Ana'];
+
+console.log(names.includes('Oscar'))
