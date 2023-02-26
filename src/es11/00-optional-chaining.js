db34b17bdebffa9af28bd977bbd41adc013ee066
Heldeg <edhernandezfo@unal.edu.co>
2023-02-26 16:54:11 -0500

    optional

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,12 @@
+const users ={ 
+    gndx: {
+        country: 'MX'
+    },
+    ana: {
+        country: 'CO'
+    }
+}
+
+console.log(users.bebeloper?.country);
+
+// usar solo en donde lo necesitas
