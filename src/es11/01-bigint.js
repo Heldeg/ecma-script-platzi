3d57e72da7129cd75350bd55aaf07cbedcb5e18c
Heldeg <edhernandezfo@unal.edu.co>
2023-02-27 10:27:38 -0500

    bigint and nullish

---
+++ src/es11/01-bigint.js
@@ -0,0 +1,6 @@
+//adds a n at end
+const aBigNumber = 8907245920742093847n;
+const anotherBigNumber = BigInt(8907245920742093847);
+
+console.log(aBigNumber),
+console.log(anotherBigNumber);
---
+++ src/es11/02-nullish.js
@@ -0,0 +1,4 @@
+//check if the value is null. If is null it asign the indicated value
+const anotherNumber =  null;
+const validate = anotherNumber ?? 5;
+console.log(validate);
